cd39da69fc307995a633aae897e0d90c0be27c7b
Legend94rz <legend94rz@gmail.com>
2015-05-16 20:56:49 +0800

    合并完成

--- app/src/main/java/org/helloworld/RegisterAct.java
+++ app/src/main/java/org/helloworld/RegisterAct.java
@@ -12,6 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +21,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -29,6 +31,8 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
 
 
 public class RegisterAct extends Activity {
@@ -36,84 +40,137 @@ public class RegisterAct extends Activity {
     private static final int PHOTO_REQUEST = 1;
     private static final int CAMERA_REQUEST = 2;
     private static final int PHOTO_CLIP = 3;
-
-    private ImageView Avatarimg;
-    private ProgressBar checkname;
-    private ProgressBar checkpassword;
-    private EditText User_name;
-    private EditText passwords;
-    private EditText confirmpasswords;
-    private Button next_step;
-    private RadioButton male;
-    private RadioButton female;
+    private ImageView iv2;
+    private ImageView ivAvatarimg;
+    private ProgressBar pbcheckname;
+    private ProgressBar pbcheckpassword;
+    private EditText etUser_name;
+    private EditText etpasswords;
+    private EditText etconfirmpasswords;
+    private Button bnext_step;
+    private RadioButton rbmale;
+    private RadioButton rbfemale;
     private Bitmap photo;
+    private ImageView ivUsernameError;
+    private ImageView ivPassError;
+    private TextView tvError_info_username;
+    private TextView tvError_info_password;
+    private boolean isRepeated=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        Avatarimg=(ImageView) findViewById(R.id.Avatar);
-        User_name=(EditText) findViewById(R.id.username);
-        passwords=(EditText) findViewById(R.id.password);
-        confirmpasswords= (EditText) findViewById(R.id.confirmPassword);
-        checkname=(ProgressBar) findViewById(R.id.check_name);
-        checkpassword=(ProgressBar)findViewById(R.id.check_password);
-        final EditText nick_name=(EditText)findViewById(R.id.nickname);
-        male=(RadioButton)findViewById(R.id.maleButton);
-        female=(RadioButton)findViewById(R.id.femaleButton);
-        next_step =(Button) findViewById(R.id.next);
+        setContentView(R.layout.register);
+        ivAvatarimg =(ImageView) findViewById(R.id.Avatar);
+        etUser_name =(EditText) findViewById(R.id.username);
+        etpasswords =(EditText) findViewById(R.id.password);
+        etconfirmpasswords = (EditText) findViewById(R.id.confirmPassword);
+        pbcheckname =(ProgressBar) findViewById(R.id.check_name);
+        iv2=(ImageView) findViewById(R.id.imageView2);
+        final EditText etnick_name=(EditText)findViewById(R.id.nickname);
+        rbmale =(RadioButton)findViewById(R.id.maleButton);
+        rbfemale =(RadioButton)findViewById(R.id.femaleButton);
+        bnext_step =(Button) findViewById(R.id.next);
+        ivPassError = (ImageView) findViewById(R.id.password_error);
+        ivUsernameError =(ImageView) findViewById(R.id.username_error);
+        tvError_info_password =(TextView) findViewById(R.id.error_info_password);
+        tvError_info_username=(TextView)findViewById(R.id.error_info_username);
         /** To set the user's avatar by choosing image fromm gallery or taking photo
          * **/
-        Avatarimg.setOnClickListener(new setAvatar());
+        ivAvatarimg.setOnClickListener(new setAvatar());
         /**To check if the user name has bend used.
          * */
-        User_name.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        etUser_name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View v,boolean hasfocus){
-                if(!hasfocus){
-                    String urname= User_name.getText().toString();
-                    Check_online task= new Check_online(urname);
+            public void onFocusChange(View v, boolean hasfocus) {
+                if (!hasfocus) {
+
+                    String urname = etUser_name.getText().toString();
+                    Check_online task = new Check_online(urname);
                     task.execute();
+                    String error_info_username="";
+                    if(isRepeated) error_info_username+=" 用户名已存在 ";
+                    boolean isLegal=true;
+                    for (int i=0;i<urname.length();i++)
+                    {
+                        int s =(int) urname.charAt(i);
+                        if(!((s>47)&&(s<58)||((s>64)&&(s<91))||((s>96)&&(s<122))||(s==95)))
+                            isLegal=false;
+                    }
+                    if (!isLegal) error_info_username+=" 用户名只能包含字母数字和下划线 ";
+                    if(urname.length()>16) error_info_username+="用户名不能超过16位";
+                    if(error_info_username.length()>0)
+                    {
+                        ivUsernameError.setVisibility(View.VISIBLE);
+                        tvError_info_username.setText(error_info_username);
+                    }else
+                    {
+                        ivUsernameError.setVisibility(View.INVISIBLE);
+                        tvError_info_username.setText("");
+                    }
                 }
             }
         });
         /** To check if the password is the same as the confirmed_password.*/
-        confirmpasswords.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        etconfirmpasswords.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View v,boolean hasfocus){
-                if(!hasfocus)
-                {
-                    String ps=passwords.getText().toString();
-                    String cmps=confirmpasswords.getText().toString();
-                    if(ps.hashCode()==cmps.hashCode())
-                    {
-                        checkpassword.setVisibility(View.GONE);
+            public void onFocusChange(View v, boolean hasfocus) {
+                if (!hasfocus) {
+                    String ps = etpasswords.getText().toString();
+                    String cmps = etconfirmpasswords.getText().toString();
+                    String error_info_password="";
+                    if (ps.hashCode() == cmps.hashCode()) {
+                       // pbcheckpassword.setVisibility(View.INVISIBLE);
+                    } else {
+                       // pbcheckpassword.setVisibility(View.INVISIBLE);
+                        error_info_password+=" 两次输入的密码不一致 ";
                     }
-                    else{
-                        checkpassword.setVisibility(View.VISIBLE);
+                    if(!((ps.length()>=8)&&(ps.length()<=32))) error_info_password+=" 密码长度为8~32位";
+                    if(error_info_password.length()>0) {
+                        ivPassError.setVisibility(View.VISIBLE);
+                        tvError_info_password.setText(error_info_password);
                     }
+                } else {
+                   // pbcheckpassword.setVisibility(View.VISIBLE);
+                    ivPassError.setVisibility(View.INVISIBLE);
+                    tvError_info_password.setText("");
                 }
-                else {
-                    checkpassword.setVisibility(View.VISIBLE);
-                }
-                }
+            }
 
         });
         /**To send to info to webservice and to register.
          * */
-        next_step.setOnClickListener(new View.OnClickListener() {
+        bnext_step.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            String Username=User_name.getText().toString();
-            String psw=passwords.getText().toString();
-            boolean usergender=male.isChecked();
-            String nick=nick_name.getText().toString();
-            Register_online task =new Register_online(Username,psw,usergender,nick);
-            task.execute();
-            String path=Global.PATH.HeadImg;
-            String filename=Global.mySelf.username+".png";
+                String Username = etUser_name.getText().toString();
+                String psw = etpasswords.getText().toString();
+                boolean usergender = rbmale.isChecked();
+                String nick = etnick_name.getText().toString();
+                Register_online task = new Register_online(Username, psw, usergender, nick);
+                task.execute();
+                String path = Global.PATH.HeadImg;
+                String filename = Global.mySelf.username + ".png";
+                //Store the photo in local address.
+                File file = new File(Global.PATH.HeadImg,etUser_name.getText().toString()+".png");
 
-            Upload_head task2 =new Upload_head(photo,path,filename);
-            task2.execute();
+                if(file.exists())
+                    file.delete();
+                try {
+                    file.createNewFile();
+                }catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                try{
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    photo.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Upload_head task2 = new Upload_head(photo, path, filename);
+                task2.execute();
 
             }
         });
@@ -127,8 +184,8 @@ public class RegisterAct extends Activity {
      public void onClick(View v)
     {
         AlertDialog.Builder builder=new AlertDialog.Builder(RegisterAct.this)
-                .setTitle("Choose the way to select avatar.")
-                .setMessage("Stupid");
+                .setTitle("请选择头像图片的来源");
+
         builder.setPositiveButton("相册",new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -163,10 +220,14 @@ public class RegisterAct extends Activity {
             try
             {
                 SoapObject result = check.call();
-                if(result.getPropertyCount()>0)
-                return Boolean.parseBoolean(result.getProperty(0).toString());
-                else
-                return true;
+                if(result.getPropertyCount()>0) {
+                    isRepeated=true;
+                    return Boolean.parseBoolean(result.getProperty(0).toString());
+                }
+                else {
+                    isRepeated=false;
+                    return true;
+                }
             }
             catch (NullPointerException e)
             {
@@ -180,14 +241,14 @@ public class RegisterAct extends Activity {
 
             if(aBoolean)
             {
-                checkname.setVisibility(View.GONE);
+                //pbcheckname.setVisibility(View.GONE);
                 //Toast.makeText(RegisterAct.this, "用户名合法", Toast.LENGTH_SHORT).show();
                 Global.mySelf.username=Username;
             }
             else
             {
                 //Toast.makeText(RegisterAct.this,"重复的用户名",Toast.LENGTH_SHORT).show();
-                checkname.setVisibility(View.VISIBLE);
+                //pbcheckname.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -215,6 +276,8 @@ public class RegisterAct extends Activity {
             try
             {
                 SoapObject result = register.call();
+
+
                 return (result.getProperty(0).toString().equals(Global.OPT_SUCCEED));
             }
             catch (NullPointerException e)
@@ -233,7 +296,8 @@ public class RegisterAct extends Activity {
                 Toast.makeText(RegisterAct.this, "注册成功", Toast.LENGTH_SHORT).show();
                 Global.mySelf.username=Username;
                 Global.mySelf.password=Password;
-                User_name.setText("okay");
+
+                etUser_name.setText("okay");
                 Intent i=new Intent(RegisterAct.this,MainActivity.class);
                 startActivity(i);
                 finish();
@@ -241,7 +305,7 @@ public class RegisterAct extends Activity {
             else
             {
                 //Toast.makeText(RegisterAct.this,"注册失败",Toast.LENGTH_SHORT).show();
-                User_name.setText("wrong");
+                etUser_name.setText("wrong");
             }
         }
     }
@@ -265,15 +329,24 @@ public class RegisterAct extends Activity {
         {
             WebService register=new WebService("uploadFile");
             byte[] StandardHead;
+            try{
             ByteArrayOutputStream binaryImg = new ByteArrayOutputStream();
             Head.compress(Bitmap.CompressFormat.PNG,100,binaryImg);
+            binaryImg.flush();
+            binaryImg.close();
             StandardHead =binaryImg.toByteArray();
-            register.addProperty("file",StandardHead).addProperty("path",Path).addProperty("fileName",Filename);
+            String result=null;
+            result = Base64.encodeToString(StandardHead,Base64.DEFAULT);
+            register.addProperty("base64",result).addProperty("path",Path).addProperty("fileName",Filename);}
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
             try
             {
                 SoapObject result = register.call();
-                String rr=result.getProperty(0).toString();
-                User_name.setText(rr);
+
+
                 return (result.getProperty(0).toString().equals(Global.OPT_SUCCEED));
             }
             catch (NullPointerException e)
@@ -288,14 +361,11 @@ public class RegisterAct extends Activity {
 
             if(aBoolean)
             {
-
                 Toast.makeText(RegisterAct.this, "上传成功", Toast.LENGTH_SHORT).show();
-
             }
             else
             {
                 Toast.makeText(RegisterAct.this,"上传失败",Toast.LENGTH_SHORT).show();
-                User_name.setText("wrong");
             }
         }
     }
@@ -343,33 +413,7 @@ public class RegisterAct extends Activity {
                     if (extras != null) {
                         // Here I switch the photo to global var
                         photo = extras.getParcelable("data");
-                        Avatarimg.setImageBitmap(photo);
-                        File file = new File(Global.PATH.HeadImg,"avatar.png");
-                        if(!file.exists())
-                            file.mkdirs();
-                        try {
-                            FileOutputStream fileOutputStream = new FileOutputStream(file.getPath());
-                            photo.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
-                            FileOutputStream fileOutputStream1 = new FileOutputStream(Global.PATH.HeadImg);
-                            photo.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream1);
-                            fileOutputStream.close();
-                            //Anther way to store the pic
-                            BufferedOutputStream bos= null;
-                            FileOutputStream fos=null;
-                            ByteArrayOutputStream baos=new ByteArrayOutputStream();
-                            photo.compress(Bitmap.CompressFormat.PNG,100,baos);
-                            byte[] byteArray=baos.toByteArray();
-                            fos = new FileOutputStream(file);
-                            bos =new BufferedOutputStream(fos);
-                            bos.write(byteArray);
-
-                            System.out.println("saveBmp is here");
-                            fos.close();
-                            baos.close();
-                            bos.close();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        ivAvatarimg.setImageBitmap(photo);
 
                     }
                 }
@@ -406,7 +450,7 @@ public class RegisterAct extends Activity {
         if (extras != null) {
             Bitmap photo = extras.getParcelable("data");
             Drawable drawable = new BitmapDrawable(photo);
-            Avatarimg.setImageDrawable(drawable);
+            ivAvatarimg.setImageDrawable(drawable);
         }
     }
     @Override
