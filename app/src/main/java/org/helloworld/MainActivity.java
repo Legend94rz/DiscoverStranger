3f74c2bccd256474833c0c98c2a6393c5851cb87
Lunar_one <lunar_one@nuaa.edu.cn>
2015-05-18 22:59:24 +0800

    按两次退出主界面

--- app/src/main/java/org/helloworld/MainActivity.java
+++ app/src/main/java/org/helloworld/MainActivity.java
@@ -493,4 +493,26 @@ public class MainActivity extends Activity implements View.OnClickListener, View
 		Intent I = new Intent(this, NearbyStrangerAct.class);
 		startActivity(I);
 	}
+    //按两次退出主界面
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN)
+        {
+
+            if((System.currentTimeMillis()-exitTime) > 2000)  //System.currentTimeMillis()无论何时调用，肯定大于2000
+            {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序",Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            }
+            else
+            {
+                finish();
+                System.exit(0);
+            }
+
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
